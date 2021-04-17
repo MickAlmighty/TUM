8885926c204124dc7a257ec095f3bf293ff52696
MickAlmighty <michalha1174@gmail.com>
2021-04-17 13:14:05 +0200

    MVVM division in Presentation, Added RelayCommand and AbstractViewModel

---
+++ Presentation/ViewModel/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Presentation.ViewModel
+{
+    /// <summary>
+    ///     Acts as a base for ViewModels.
+    /// </summary>
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        ///     Raises PropertyChanged event with appropriate property name. Used to update bindings.
+        /// </summary>
+        /// <param name="propertyName">
+        ///     Exact name of the property which was modified.
+        ///     If this argument is omitted, it's replaced with the name of the calling property/method.
+        /// </param>
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
